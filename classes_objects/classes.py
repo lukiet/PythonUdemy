66801a964c92ba7d67611612c31465f55dd932a0
lukiet <luketsalwa@gmail.com>
2025-07-23 12:27:28 +0300

    classes and objects

--- classes_objects/classes.py
+++ classes_objects/classes.py
@@ -10,6 +10,15 @@ class ComplexNumber:
 
 #creating a new object
 complex1 = ComplexNumber(30,50)
+complex2 = ComplexNumber(23,90)
 
 #calling display function
 complex1.display_complex()
+complex2.display_complex()
+
+#new object plus new attribute
+complex3 = ComplexNumber(91,23)
+complex3.new_attribute = 56
+
+complex3.display_complex()
+print(complex3.real_part, complex3.imag_part, complex3.new_attribute)
