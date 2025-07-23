9e60a210e796bbcb2a3671b70d5440ccd306ea3e
lukiet <luketsalwa@gmail.com>
2025-07-23 12:45:05 +0300

    arrays

---
+++ Arrays/arrays.py
@@ -0,0 +1,17 @@
+fruits = ["Mango","Orange","Apple","Grape","Berry"]
+
+print(fruits)
+print(fruits[2])
+print(len(fruits))
+
+#adding a new item to array
+fruits.append("Banana")
+print(fruits)
+
+#removing an item from the array
+cars = ["Toyota","Nissan","Mazda","Tesla","Honda","Mobius"]
+print(cars)
+del cars[1]
+cars.remove("Tesla")
+cars.pop(3)
+print(cars)
