fdab1038aaeaa915e58d8e97325049d37518dfc3
lukiet <luketsalwa@gmail.com>
2025-07-24 21:55:32 +0300

    directory

--- DirectorynFilemanagement/directory.py
+++ DirectorynFilemanagement/directory.py
@@ -1,3 +1,15 @@
 import os
 print(os.getcwd())
 print(os.getcwdb())
+
+#show contents of current directory
+print(os.listdir())
+
+# Create a new directory named 'Test'
+os.mkdir('Test')
+
+print(os.listdir())
+
+# Rename the directory 'Test' to 'Newt'
+os.rename('Test', 'Newt')
+print(os.listdir())
