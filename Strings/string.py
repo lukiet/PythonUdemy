a81738d6032bb0498e082e6ac47407cdd76bf821
lukiet <luketsalwa@gmail.com>
2025-07-24 23:38:55 +0300

    strings

--- Strings/string.py
+++ Strings/string.py
@@ -1,4 +1,4 @@
-name = 'Kanairo'
+name = 'Kanairo Mji Wetu'
 print(name)
 
 print(name[0:4])
@@ -7,3 +7,21 @@ print(name[0:4:2])
 
 for i in name:
     print(i)
+
+# Escaping single quotes
+print('I\'m a student')
+
+# Escaping double quotes
+print("He said, \"Hello!\"")
+
+# New line character
+print("Hello\nWorld")
+
+# Tab character
+print("Hello\tWorld")
+
+# String methods
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.find('Mj'))
