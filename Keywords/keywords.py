2132aaf22b53e7d03afe18a63303af82dc7b98c6
lukiet <luketsalwa@gmail.com>
2025-07-24 11:56:48 +0300

    keywords

--- Keywords/keywords.py
+++ Keywords/keywords.py
@@ -6,3 +6,25 @@ print(2>2)
 a = [1,2,3,4,5]
 print(2 in a)
 print(7 not in a)
+
+#is
+print(True is True)
+print(True is False)
+
+# lambda
+a = lambda x: x*2
+for i in range(1,20):
+    print(a(i))
+
+#non local
+def outer_function():
+    a = 5
+    def inner_function():
+        nonlocal a
+        a = 10
+        print('Inner function:', a)
+    inner_function()
+    print('Outer function:', a)
+
+outer_function()
+
