ab62ec5d11ddb76c47d645da1bbfe360a06ecbab
lukiet <luketsalwa@gmail.com>
2025-07-24 21:00:28 +0300

    sets

--- Sets/sets.py
+++ Sets/sets.py
@@ -10,3 +10,13 @@ print(guesses)
 print(type(guesses))
 print(guesses.pop())
 print(guesses)
+
+# Set operations
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set2.difference(set1))
+print(set1.symmetric_difference(set2))
