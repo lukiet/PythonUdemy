9382c2ddd6a3be1fb995e2a98974fd362490d3f8
lukiet <luketsalwa@gmail.com>
2025-07-28 14:50:14 +0300

    Conversion

--- DataTypeConversion/data.py
+++ DataTypeConversion/data.py
@@ -10,5 +10,14 @@ print(temp3)
 print(type(temp3))
 
 # Explicit type conversion
-temp4 = '33.9'
+temp4 = '33'
+temp6 = '30.8'
 print(type(temp4))
+print(type(temp6))
+
+temp5 = int(temp4)
+temp7 = float(temp6)
+
+print(temp5)
+print(temp7)
+print(type(temp7))
