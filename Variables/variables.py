7a0adb5fb12e8eb82befda12511822a8dac132dd
lukiet <luketsalwa@gmail.com>
2025-07-28 21:10:18 +0300

    Variables

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,3 +1,4 @@
+# Global and Local Variables with different names
 x = 'global'
 
 def funct1():
@@ -10,3 +11,29 @@ def funct1():
 print('Global x = ', x)
 funct1()
 print('Global x = ', x)
+
+# Global and Local Variables with same names
+
+a = 5
+
+def funct2():
+    a = 10
+    print('Local a = ', a)
+
+print('Global a = ', a)
+funct2()
+print('Global a = ', a)
+
+# Creating and using a non-local variable
+def outer_function():
+    x = "local"
+
+    def inner_function():
+        nonlocal x
+        x = "non-local"
+        print("Inner:", x)
+
+    inner_function()
+    print("Outer:", x)
+
+outer_function()
