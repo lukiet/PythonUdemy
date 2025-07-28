3acb7627a3c2646ac5baf9b985971af1c607c4bd
lukiet <luketsalwa@gmail.com>
2025-07-28 20:52:23 +0300

    Namespace

--- NamespaceScope/namespace.py
+++ NamespaceScope/namespace.py
@@ -1,3 +1,5 @@
+# namespaces show where a variable is stored in memory
+
 a = 2
 b = 45
 c = a + b
@@ -6,3 +8,22 @@ print(id(2))
 print(id(a))
 print(id(b))
 print(id(c))
+
+# scope is the region of the program where a variable is accessible
+def temp():
+    global a
+    a = 34
+
+    def inner():
+        global a
+        a = 45
+        print("a = ", a)
+
+    inner()
+    print("a = ", a)
+
+a = 8
+print('a = ', a)
+temp()
+print('a = ', a)
+
