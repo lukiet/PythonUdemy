f2887a9ec8188f935250c261b9ed39596ed13977
lukiet <luketsalwa@gmail.com>
2025-07-28 15:03:06 +0300

    Numbers

---
+++ Numbers/numbers.py
@@ -0,0 +1,11 @@
+num1 =100
+num2 = 100.78
+num3 = 100 + 200j
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+print(isinstance(num1, int))
+print(isinstance(num2, float))
+print(isinstance(num3, complex))
