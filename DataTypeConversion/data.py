d6971d3d947e24fc1648b275561b8b005c4d0f58
lukiet <luketsalwa@gmail.com>
2025-07-28 14:46:15 +0300

    Conversion

---
+++ DataTypeConversion/data.py
@@ -0,0 +1,14 @@
+# Implicit type conversion
+
+temp1 = 32
+temp2 = 35.6
+temp3 = temp1 + temp2
+
+print(type(temp1))
+print(type(temp2))
+print(temp3)
+print(type(temp3))
+
+# Explicit type conversion
+temp4 = '33.9'
+print(type(temp4))
