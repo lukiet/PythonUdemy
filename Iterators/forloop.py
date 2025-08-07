e2c4e51b55b325062bdca86b2e51d7afd9fde8ae
lukiet <luketsalwa@gmail.com>
2025-08-07 15:43:37 +0300

    for loops

---
+++ Iterators/forloop.py
@@ -0,0 +1,10 @@
+# Printing all elements of a list using a for loop
+my_list = [1, 2, 3, 4, 5]
+for item in my_list:
+    print(item)
+
+# Using range
+for i in range(5):
+    print(i)
+else:
+    print("Loop completed without interruption.")
