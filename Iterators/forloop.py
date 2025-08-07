5042923db380682961559f48947b627d566dfd4d
lukiet <luketsalwa@gmail.com>
2025-08-07 15:52:39 +0300

    inheritance

--- Iterators/forloop.py
+++ Iterators/forloop.py
@@ -1,10 +1,11 @@
 # Printing all elements of a list using a for loop
 my_list = [1, 2, 3, 4, 5]
 for item in my_list:
-    print(item)
+    print(item, end=' ')
+    print(' Number')
 
 # Using range
-for i in range(5):
+for i in range(1,20,3):
     print(i)
 else:
     print("Loop completed without interruption.")
